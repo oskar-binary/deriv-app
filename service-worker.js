876f67318e464a4a1ea0c31ade2e3375f05a5b42
oskar-binary <oskar@binary.com>
2020-04-13 12:32:00 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fe44016ced192ca68f5886e6e1f251c.js"
+  "/precache-manifest.90f590a7867fb5493c29122b57e843e0.js"
 );
 
 workbox.core.skipWaiting();
